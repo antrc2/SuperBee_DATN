efee258e8d380194f903120e3a2233654dfbb803
yennth2005 <ngohaiyen1237a6@gmail.com>
2025-05-17 19:47:51 +0700

    create view category management

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,12 @@
-import Home from "@pages/Clients/Home/Home.jsx";
+// src/App.jsx
+import { useRoutes } from "react-router-dom";
+import Home from "@pages/Clients/Home/Home";
+import adminRoutes from "@routers/Admin";
 
-function App() {
-  return <Home />;
+export default function App() {
+  const routes = useRoutes([
+    { path: "/", element: <Home /> },
+    ...adminRoutes,
+  ]);
+  return routes;
 }
-export default App;

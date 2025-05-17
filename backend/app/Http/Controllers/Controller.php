6da3ef3de2273ffd09f69250be7caee5a0a19447
antrc2 <antrc2gamer@gmail.com>
2025-05-17 09:13:59 +0700

    add abstract class controller

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -4,5 +4,10 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    abstract public function getAll();
+    abstract public function getOne();
+    abstract public function post();
+    abstract public function put();
+    abstract public function delete();
+    abstract public function patch();
 }

deca76443d975547f5e504d84822b4877583326b
thaiph <thaiphph52804@gmail.com>
2025-05-18 15:28:45 +0700

    upload

--- backend/app/Http/Middleware/JWTMiddleware.php
+++ backend/app/Http/Middleware/JWTMiddleware.php
@@ -22,16 +22,18 @@ class JWTMiddleware
         $jwt = $matches[1];
 
         try {
-            $secretKey = env('JWT_SECRET_KEY', 'your-secret-key');
+            $secretKey = env('JWT_SECRET', 'your-secret-key');
             $decoded = JWT::decode($jwt, new Key($secretKey, 'HS256'));
 
             // Ví dụ trong payload có trường api_key
             if (!isset($decoded->api_key)) {
                 return response()->json(['error' => 'API key not found in token'], 401);
             }
-            
-            $apiKey = $decoded->api_key;
 
+            if (preg_match('/KEY\s(\S+)/', $decoded->api_key, $matches)) {
+                $apiKey = $matches[1];
+            }
+            // dd($apiKey);
             $webRecord = Web::where('api_key', $apiKey)->first();
 
             if (!$webRecord) {
@@ -39,8 +41,7 @@ class JWTMiddleware
             }
 
             // Thêm web_id vào request
-            $request->attributes->add(['web_id' => $webRecord->web_id]);
-
+            $request->merge(['web_id' => $webRecord->id, 'userId' => $decoded->user_id]);
         } catch (\Exception $e) {
             return response()->json(['error' => 'Invalid or expired token', 'message' => $e->getMessage()], 401);
         }

f34e5aa4834fcd9d97fe6d0f7b4b2791fa49e1e8
antrc2 <96590097+antrc2@users.noreply.github.com>
2025-05-20 16:05:35 +0700

    Feature/account management (#52)
    * create route, controller, model account-management
    
    * fix conflicts
    
    ---------
    
    Co-authored-by: admin-kazuto <147340489+admin-kazuto@users.noreply.github.com>

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,10 +14,7 @@ use App\Http\Controllers\HomeController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\AccountController;
-
-
+use App\Http\Controllers\UserController;
 
 Route::middleware(['authenticate'])->group(function () {
 
@@ -114,24 +111,14 @@ Route::middleware(['jwt'])->group(function () {
         Route::patch("/{id}");
         Route::delete("/{id}");
     });
-<<<<<<< HEAD
-    Route::middleware(['jwt'])->prefix("/accounts")->group(function () {
-        Route::get("/", [AccountController::class, 'index']);
-        Route::post("/", [AccountController::class, 'store']); // Tạo mới
-        Route::get("/{id}", [AccountController::class, 'show']); // Chi tiết   
-        Route::put("/{id}", [AccountController::class, 'update']); // cập nhật toàn bộ
-        Route::patch("/{id}", [AccountController::class, 'restore']); // cập nhật 1 phần 
-        Route::delete("/{id}", [AccountController::class, 'destroy']); // xóa mềm 
-=======
+
     Route::prefix("/accounts")->group(function () {
-        Route::post('/logout', [AuthController::class, 'logout']);
-        Route::get("/");
-        Route::get("/{id}");
-        Route::post("/");
-        Route::put("/{id}");
-        Route::patch("/{id}");
-        Route::delete("/{id}");
->>>>>>> 068a65f30c020bebf3750845f2725c1be17e7501
+        Route::get("/", [UserController::class, 'index']);
+        Route::post("/", [UserController::class, 'store']); // Tạo mới
+        Route::get("/{id}", [UserController::class, 'show']); // Chi tiết   
+        Route::put("/{id}", [UserController::class, 'update']); // cập nhật toàn bộ
+        Route::patch("/{id}", [UserController::class, 'restore']); // cập nhật 1 phần 
+        Route::delete("/{id}", [UserController::class, 'destroy']); // xóa mềm 
     });
     Route::prefix("/cart")->group(function () {
         Route::get("/");

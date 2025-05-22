e47feeced370fc884a04d9737c3b3e3801a50dbb
antrc2 <antrc2gamer@gmail.com>
2025-05-22 14:20:42 +0700

    Update Destroy Function

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     $this->call([
         WebSeeder::class,
         RoleSeeder::class,
-        UserSeeder::class
+        UserSeeder::class,
+        CategorySeeder::class
     ]);
     }
 }

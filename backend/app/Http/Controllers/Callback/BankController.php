7dda506c240ce518514207784a62ce51a22a3d60
antrc2 <antrc2gamer@gmail.com>
2025-06-08 00:15:59 +0700

    add card callback and process donate promotion if exists

--- backend/app/Http/Controllers/Callback/BankController.php
+++ backend/app/Http/Controllers/Callback/BankController.php
@@ -7,7 +7,13 @@ use Illuminate\Http\Request;
 
 class BankController extends Controller
 {
-    public function index(Request $request){
-        
+    public function index(Request $request)
+    {
+        if ($request->token !== env("BANK_TOKEN")) {
+            return response()->json([
+                "status" => False,
+                "message" => "Can't access to here",
+            ], 403);
+        }
     }
 }

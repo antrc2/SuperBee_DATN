494f8c6fc5dd30948bbf6b9b7e943c50bb3e0edb
TranHai15 <hairobet15092005@gmail.com>
2025-06-11 18:34:36 +0700

    fix lại giao diện client hiển thị (#146)

--- Frontend/src/sections/Home/ListCategoryCon.jsx
+++ Frontend/src/sections/Home/ListCategoryCon.jsx
@@ -3,15 +3,14 @@ import CategoryCon from "../../components/Client/Category/CategoryCon";
 export default function ListCategoryCon({
   items = [],
   count = 5,
-  title = "Danh mục"
+  title = "Danh mục",
 }) {
-  const displayedItems = items.slice(0, count); // Chỉ lấy đúng số lượng
+  if (!items.length || !items[0]?.children) return null; // Kiểm tra nếu không có dữ liệu
+  const displayedItems = items[0]?.children.slice(0, count); // Chỉ lấy đúng số lượng
 
   return (
-
     <div className="w-full  max-w-7xl mx-auto ">
       <div className="flex justify-between items-center mb-3">
-
         <h2 className="text-2xl font-bold mb-3 flex items-center gap-2">
           {title}
         </h2>
@@ -21,7 +20,6 @@ export default function ListCategoryCon({
       </div>
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
-
         {displayedItems.map((item, index) => (
           <CategoryCon key={index} item={item} />
         ))}
--- backend/app/Http/Controllers/User/UserProductController.php
+++ backend/app/Http/Controllers/User/UserProductController.php
@@ -3,57 +3,78 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class UserProductController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request, $slug)
+    {
         try {
-            $products = Product::with('category')->with("images")->with("gameAttributes")->where('status',1)->get();
+            $category = Category::where('slug', $slug)
+                ->where('status', 1)
+                ->first();
+        
+            if (!$category) {
+                return response()->json(
+                    [
+                        "status" => False,
+                        "message" => "Không tìm thấy danh mục sản phẩm",
+                        "data" => []
+                    ],
+                    404
+                );
+            }
+            $products = Product::with("images")->with('category')->with("gameAttributes")->where('status', 1)->where('category_id', $category->id)->get();
             return response()->json(
                 [
-                    "status"=>True,
-                    "message"=>"Lấy danh sách sản phẩm thành công",
-                    "data"=>$products
+                    "status" => True,
+                    "message" => "Lấy danh sách sản phẩm thành công",
+                    "data" => [
+                        "category" => $category,
+                        "products" => $products
+                    ]
                 ]
-                );
+            );
         } catch (\Throwable $th) {
             return response()->json(
                 [
-                    "status"=>True,
-                    "message"=>"Lấy danh sách sản phẩm thất bại",
-                    "data"=>[]
+                    "status" => True,
+                    "message" => "Lấy danh sách sản phẩm thất bại",
+                    "data" => []
                 ]
             );
         }
     }
-    public function show(Request $request, $id){
+    public function show(Request $request, $id)
+    {
         try {
-            $product = Product::with('category')->with("images")->with("gameAttributes")->where('status',1)->where('id',$id)->get();
+            $product = Product::with('category')->with("images")->with("gameAttributes")->where('status', 1)->where('sku', $id)->get();
             if (count($product) == 0) {
                 return response()->json(
                     [
-                        "status"=>false,
-                        "message"=>"Không tìm thấy sản phẩm",
-                        "data"=> []
-                    ],404
+                        "status" => false,
+                        "message" => "Không tìm thấy sản phẩm",
+                        "data" => []
+                    ],
+                    404
                 );
             }
 
             return response()->json(
                 [
-                    "status"=>True,
-                    "message"=>"Xem chi tiết sản phẩm thành công",
-                    "data"=>$product
+                    "status" => True,
+                    "message" => "Xem chi tiết sản phẩm thành công",
+                    "data" => $product
                 ]
-                );
+            );
         } catch (\Throwable $th) {
             return response()->json([
-                "status"=>False,
-                "message"=>"Đã có lỗi xảy ra",
-                'data'=>[]
-            ],500);
+                "status" => False,
+                "message" => "Đã có lỗi xảy ra",
+                'data' => []
+            ], 500);
         }
     }
 }

b063af14fe915bf02bbf49eafbcd9f67de59b64d
TranHai <hairobet15092005@gmail.com>
2025-06-11 12:55:56 +0700

    fix lại middlware giao diện

--- Frontend/src/components/common/ProtectedRoute.jsx
+++ Frontend/src/components/common/ProtectedRoute.jsx
@@ -4,13 +4,6 @@ import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { hasRequiredRole } from "@utils/tokenUtils";
 
-/**
- * Component "gác cổng" để bảo vệ các route.
- *
- * @param {object} props
- * @param {React.ReactNode} props.children - Component con sẽ được render nếu được phép.
- * @param {string[]} props.allowedRoles - Mảng các vai trò được phép truy cập.
- */
 const ProtectedRoute = ({ children, allowedRoles }) => {
   const location = useLocation();
   const isAllowed = hasRequiredRole(allowedRoles);
@@ -19,7 +12,7 @@ const ProtectedRoute = ({ children, allowedRoles }) => {
     // Người dùng không có quyền.
     // Chuyển hướng họ đến trang đăng nhập.
     // `state={{ from: location }}` giúp chuyển hướng người dùng trở lại trang họ muốn sau khi đăng nhập thành công.
-    return <Navigate to="/" state={{ from: location }} replace />;
+    return <Navigate to="/not-authorized" state={{ from: location }} replace />;
   }
 
   // Nếu có quyền, render component con

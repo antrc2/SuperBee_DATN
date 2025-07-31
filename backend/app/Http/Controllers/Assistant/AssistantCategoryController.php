c256e9f16c6338e46e11ab179f53e579c2338eb2
antrc2 <antrc2gamer@gmail.com>
2025-07-31 14:46:44 +0700

    fix

--- backend/app/Http/Controllers/Assistant/AssistantCategoryController.php
+++ backend/app/Http/Controllers/Assistant/AssistantCategoryController.php
@@ -33,11 +33,12 @@ class AssistantCategoryController extends Controller
     public function show(Request $request,$id){
         try {
             $category = Category::where('id',$id)->with('products.gameAttributes')->with("products.images")->get();
+            $frontend_link = env("FRONTEND_URL");
             return response()->json([
                 'status'=>False,
                 'message'=>"Lấy danh sách sản phẩm theo id danh mục {$id} thành công",
                 'data'=>$category,
-                "link"=>"http://localhost:5173/mua-acc/{$category->slug}"
+                "link"=>"{$frontend_link}/mua-acc/{$category->slug}"
             ]);
         } catch (\Throwable $th) {
             return response()->json([
